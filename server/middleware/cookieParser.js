3896b7058d705a59ab185950d49e0daf61ee32b5
rtownsend722 <rebecca.townsend722@gmail.com>
2017-11-07 12:11:35 -0800

    make cookie parser

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,26 @@
+// recieve a post request with a cookie property on it and the cookie is JSON stringified
 const parseCookies = (req, res, next) => {
+  //access the cookie property
+  if (req.headers.cookie) {
+    var cookies = req.headers.cookie;
+    // make a cookie object
+    var parsedCookies = {};
+    // split the req.Cookie string on '; '
+    cookies = cookies.split('; ');
+    // iterate through the resulting array
+    cookies.forEach(function(cookie) {
+      // set new array variable equal to splitting string on =
+      var individualCookie = cookie.split('=');
+      // set cookie object's key value pairs equal to ^
+      parsedCookies[individualCookie[0]] = individualCookie[1];
+      console.log('i am the parsed object ', parsedCookies);
+    });
+    console.log('i am the parsed object OUTSIDE of the forEach, ', parsedCookies);
+    // replace the existing JSON cookie on the request object with the parsed cookie 
+    req.cookies = parsedCookies;
+    // then()
+  }
+  next(req);
 };
 
 module.exports = parseCookies;
