8a6b85b9cb904e91d099b63ccffe323ebe674ec1
rtownsend722 <rebecca.townsend722@gmail.com>
2017-11-07 18:43:24 -0800

    "Clean up files"

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,6 @@ const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
   // check if there are no cookies :(
-  console.log('plush plush');
   var idExists = false;
   if (!req.cookies.shortly) {
     // if there no cookies, make a session
@@ -14,7 +13,6 @@ module.exports.createSession = (req, res, next) => {
         return models.Sessions.get({id: results.insertId});
       })
       .then( (results) => {
-        console.log('results from models.Sessions.get DONt MISLE US ', results);
         req.session = {hash: results.hash};
         res.cookies.shortlyid = {};
         res.cookies.shortlyid.value = [];
@@ -23,32 +21,21 @@ module.exports.createSession = (req, res, next) => {
         return models.Sessions.get({hash: res.cookies.shortlyid.value});
       })
       .then( (results) => {
-        console.log('results from models.Sessions.get', results);
         if (results.userId) {
           idExists = true;
         }
         return models.Users.get({id: results.userId});
       })
       .then( (results) => {
-        console.log('results from models.Users.get', results);
-        console.log('IF THIS LOGS IT WORKS', idExists);
+        console.log('IF THIS LOGS IT WORKS');
         req.session.user = {};
         if (idExists) {
-          console.log(req.session, 'this is our session ok guys');
           req.session.userId = results.id;
           req.session.user.username = results.username;
         }
         next();
       });
-        // .then
-        // check to see if we get null for results.id
-          // if we do, then next()
-        // RETURN using id query users table for username
-        // .then
-        // set the id and username on the sessions object
-        // next(); we'll be back for you
   }
-  // otherwise, console log that there are cookies :)
 };
 
 /************************************************************/
